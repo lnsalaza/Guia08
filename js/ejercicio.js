1532ca9959333c50c57d5676d9ce80307cf6b907
lnsalaza <lnsalaza@espol.edu.ec>
2022-06-20 17:23:42 -0500

    cambios

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -10,28 +10,54 @@ let cargarDatos = () => {
         for(let escritor of escritores) {
             let id = escritor.querySelector('id').textContent
             let nombre = escritor.querySelector('nombre').textContent
-            
             let plantilla = `<option value="${id}">${nombre}</option>`
             document.querySelector('select').innerHTML += plantilla
         }
     })
-  .catch(console.error);
+    .catch(console.error);
 }
+
 
 document.addEventListener('DOMContentLoaded',(event) => {
     cargarDatos()
 });
 
+const cargarFrases = async (id, nombre) => {
+    try {
+        var contenedor = document.getElementById('frases')
+        contenedor.innerHTML = '';
+        let response = await fetch('https://dataserverdaw.herokuapp.com/escritores/frases');
+        let data = await response.json();
 
-let cambio = document.querySelector('select')
-cambio.addEventListener('change', (event) => {
-    fetch("https://dataserverdaw.herokuapp.com/escritores/frases")
-    .then(response => response.json())
-    .then(data => {
-    
+        const frasesById = data.frases.filter(frase => frase.id_autor == id);
 
+        for( frase of frasesById ) {
+            let plantilla = `<div class="col-lg-3">
+                                <div class="test-inner ">
+                                    <div class="test-author-thumb d-flex">
+                                        <div class="test-author-info">
+                                            <h4>${nombre}</h4>                                            
+                                        </div>
+                                    </div>
+                                    <span>${frase.texto}</span>
+                                    <i class="fa fa-quote-right"></i>
+                                </div>
+                            </div>`
+
+            contenedor.innerHTML += plantilla
         }
 
-    })
-    .catch(console.error);
-});
+    } catch (err) {
+        console.error("Fetch err".repeat(10),err,"Fetch err".repeat(10))
+    }
+}
+
+let select = document.getElementsByTagName('select')[0];
+
+select.addEventListener('change', (event) => {
+    const id = event.target.value;
+    const nombre = event.target.options[event.target.selectedIndex].text;
+    cargarFrases(id, nombre);
+})
+
+
